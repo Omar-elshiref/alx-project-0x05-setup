865d3631f96bd31c0762217d49d421df2ec05a9d
Omar mohamed <omarabdou2002@yahoo.com>
2025-06-17 23:39:44 +0300

    Done 2

--- alx-project-0x11/next.config.ts
+++ alx-project-0x11/next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: [
+      "prlabsapi.com",
+      // أضف أي دومينات أخرى تحتاجها هنا
+    ],
+  },
 };
 
 export default nextConfig;
